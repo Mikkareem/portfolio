758e0c23327d2749f7c944c3f3b9e6697ea4bb53
Mikkareem <mohdirsath1997@gmail.com>
2024-12-25 17:39:57 +0530

    Temporary Project changes

--- src/screens/home/components/ProjectList.tsx
+++ src/screens/home/components/ProjectList.tsx
@@ -16,42 +16,42 @@ const projects: ProjectListModel[] = [
         sublistModel: []
     },
     {
-        title: 'Lotcode - A Leetcode Clone',
+        title: 'Online Chess Game',
+        thumbnailUrl: '/projects/online-chess-game/online-chess-game-thumb.jpg',
+        subtitle: 'Online Chess Game with 2 players in a Room with Real time updates using Websockets',
+        description: 'Description of Online Chess Game',
+        sublistModel: []
+    },
+    {
+        title: 'Sudoku Game/Solver',
         thumbnailUrl: '/projects/lotcode/Lotcode-thumb.jpg',
         subtitle: 'Online Code Submission platform which is inspired from Leetcode (famously known).',
         description: 'Description of Leetcode Clone',
         sublistModel: []
     },
     {
-        title: 'Lotcode - A Leetcode Clone',
+        title: 'Finance Tracker Application',
         thumbnailUrl: '/projects/lotcode/Lotcode-thumb.jpg',
         subtitle: 'Online Code Submission platform which is inspired from Leetcode (famously known).',
         description: 'Description of Leetcode Clone',
         sublistModel: []
     },
     {
-        title: 'Lotcode - A Leetcode Clone',
+        title: 'Image Editor Android Application',
         thumbnailUrl: '/projects/lotcode/Lotcode-thumb.jpg',
         subtitle: 'Online Code Submission platform which is inspired from Leetcode (famously known).',
         description: 'Description of Leetcode Clone',
         sublistModel: []
     },
     {
-        title: 'Lotcode - A Leetcode Clone',
+        title: 'Bill Generation Application (Barcode scanning - MLKit)',
         thumbnailUrl: '/projects/lotcode/Lotcode-thumb.jpg',
         subtitle: 'Online Code Submission platform which is inspired from Leetcode (famously known).',
         description: 'Description of Leetcode Clone',
         sublistModel: []
     },
     {
-        title: 'Lotcode - A Leetcode Clone',
-        thumbnailUrl: '/projects/lotcode/Lotcode-thumb.jpg',
-        subtitle: 'Online Code Submission platform which is inspired from Leetcode (famously known).',
-        description: 'Description of Leetcode Clone',
-        sublistModel: []
-    },
-    {
-        title: 'Lotcode - A Leetcode Clone',
+        title: 'Jetpack compose Mini-project Collections',
         thumbnailUrl: '/projects/lotcode/Lotcode-thumb.jpg',
         subtitle: 'Online Code Submission platform which is inspired from Leetcode (famously known).',
         description: 'Description of Leetcode Clone',
@@ -61,11 +61,13 @@ const projects: ProjectListModel[] = [
 
 const ProjectListItem = ({ project }: { project: ProjectListModel }) => {
     return (
-        <div className='bg-pink-600 flex flex-col gap-4'>
+        <div className='bg-pink-600 flex flex-col gap-4 pb-4'>
             <img src={import.meta.env.BASE_URL + project.thumbnailUrl} alt={"thumbnail"} className='w-full'/>
-            <h3>{project.title}</h3>
-            <p>{project.subtitle}</p>
-            <button>Read More</button>
+            <div className='flex flex-col gap-2 px-4 flex-grow'>
+                <h3 className='text-xl font-semibold'>{project.title}</h3>
+                <p className='font-light flex-grow'>{project.subtitle}</p>
+                <button className='bg-amber-500 text-black rounded p-2'>Read More</button>
+            </div>
         </div>
     )
 }
@@ -75,7 +77,7 @@ const ProjectList = () => {
     return (
         <div className='grid grid-cols-4 gap-4'>
             {projects.map((project) => (
-                <ProjectListItem key={project.name} project={project} />
+                <ProjectListItem key={project.title} project={project} />
             ))}
         </div>
     );
